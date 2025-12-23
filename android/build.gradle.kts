2f9cd7ddec6505b1ec7a157b4ee579ea8dc17829
Christos Toilos <christoil388@gmail.com>
2025-12-23 09:39:04 +0200

    updated build gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,20 @@
+buildscript {
+    // Define the Kotlin version here to fix the share_plus error
+    val kotlin_version = "1.9.10"
+    
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        // Classpath for the Android Gradle Plugin
+        classpath("com.android.tools.build:gradle:7.3.0")
+        // Classpath for the Kotlin Gradle Plugin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,10 +22,9 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory
+    .dir("../../build")
+    .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
